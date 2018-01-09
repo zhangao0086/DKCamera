3718674dc4f77f9b3fe2a19ea7f519e8e86a93a3
Bannings <zhangao0086@gmail.com>
2018-01-09 19:08:13 +0800

    Add Carthage support.

---
+++ DKCamera/DKCamera.h
@@ -0,0 +1,19 @@
+//
+//  DKCamera.h
+//  DKCamera
+//
+//  Created by ZhangAo on 09/01/2018.
+//  Copyright © 2018 ZhangAo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DKCamera.
+FOUNDATION_EXPORT double DKCameraVersionNumber;
+
+//! Project version string for DKCamera.
+FOUNDATION_EXPORT const unsigned char DKCameraVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKCamera/PublicHeader.h>
+
+
